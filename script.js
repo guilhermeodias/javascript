04936ba7bbaecc0975a32fb1d3bc654937ff736d
guilhermeodias <guilhermeodias@hotmail.com>
2023-05-08 19:09:08 -0300

    até7

--- script.js
+++ script.js
@@ -1,24 +1,102 @@
-let weekDay = "sábado";
+let a = 15
+let b = 38
+let c = 50
+let d = -5
 
-if (weekDay === "segunda-feira" || weekDay === "terça-feira" || weekDay === "quarta-feira" || weekDay === "quinta-feira" || weekDay === "sexta-feira") {
-    console.log("Oba, mais um dia de aprendizado na Trybe >:D");
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+console.log(a % b);
+
+if (a > b) {
+    console.log(a + " maior que " + b);
 } else {
-    console.log("FINALMENTE, descanso merecido! UwU!");
+    console.log(b + " maior que " + a);
 }
 
-let candidata = "reprovada";
+if (a > b && a > c) {
+    console.log(a + " maior valor");
+} else if (b > a && b > c) {
+    console.log(b + " maior valor");
+}else {
+    console.log(c + " maior valor");
+}
 
-switch (candidata) {
+if (d > 0) {
+    console.log("positive");
+} else if (d < 0) {
+    console.log("negative");
+} else {
+    console.log("zero");
+}
 
-    case "aprovada":
-        console.log("Parabéns, você está no grupo de pessoas aprovadas!");
-    break;
-    case "lista":
-        console.log("Você está na nossa lista de espera");
-    break;
-    case "reprovada":
-        console.log("Infelizmente, você reprovou.");
-    break;
-    default:
-        console.log("Informação incorreta.");
+let ang1 = 40
+let ang2 = 30
+let ang3 = 110
+
+if (ang1 + ang2 + ang3 === 180) {
+    console.log("true");
+} else if (ang1 + ang2 + ang3 != 180) {
+    console.log("negative");
+} else if (ang1 + ang2 + ang3 < 0) {
+    console.log("Inválido");
+}
+
+let chess = "queen";
+
+switch (chess) {
+    case "pawn":
+        console.log("peão - anda somente uma casa à frente");
+        break;
+        
+    case "rook":
+        console.log("torre - anda somente na vertical e horizontal quantas casas quiser");
+        break;
+
+    case "knight":
+        console.log("cavalo - anda 5 casas em L");
+        break;
+        
+    case "queen":
+        console.log("rainha - anda para todos os lados quantas casas desejar");
+        break;    
+
+    default: console.log("peça inválida");
+}
+
+let note = 165
+
+if (note >= 90 && note < 100) {
+    console.log("Nota A");
+} else if (note >= 80 && note < 100) {
+    console.log("Nota B");
+} else  if (note >= 70 && note < 100) {
+    console.log("Nota C");
+} else  if (note >= 60 && note < 100) {
+    console.log("Nota D");
+}  else  if (note >= 50 && note < 100) {
+    console.log("Nota E");
+} else if (note < 50 && note < 100) {
+    console.log("Nota F");
+} else if (note < 0 || note > 100) {
+    console.log("Erro de Nota");
+}
+
+let grade = 176;
+
+if (grade < 0 || grade > 100) {
+  console.log("Erro, nota incorreta!");
+} else if (grade >= 90) {
+  console.log("A");
+} else if (grade >= 80) {
+  console.log("B");
+} else if (grade >= 70) {
+  console.log("C");
+} else if (grade >= 60) {
+  console.log("D");
+} else if (grade >= 50) {
+  console.log("E");
+} else {
+  console.log("F");
 }
